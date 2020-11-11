12fa89a889c4a30db80de88a9b720a1a4cb79825
salar taeb <salartaeb10@gmail.com>
2020-11-11 06:19:42 +0000

    added bodyparser access

--- express_server.js
+++ express_server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -30,7 +34,22 @@ app.get("/set", (req, res) => {
   res.send(`a = ${a}`);
  });
 
+ app.get('/urls', (req, res) => {
+   const templateVars = { urls: urlDatabase};
+   res.render('urls_index', templateVars);
+ });
+
+ app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+ app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL:  urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
 });
+
+
