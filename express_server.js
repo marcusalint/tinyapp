3cc16d73f097b83084a2ef0cdd971da0b053fd28
salar taeb <salartaeb10@gmail.com>
2020-11-12 13:09:34 +0000

    added post method to delete link in url list

--- express_server.js
+++ express_server.js
@@ -63,7 +63,6 @@ app.post("/urls", (req, res) => {
   let longURL = req.body.longURL
   urlDatabase[shortURL] = longURL
   res.redirect(`/urls/${shortURL}`)
-  
 
 });
 
@@ -71,6 +70,20 @@ app.post("/urls", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL:  urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+
+
+});
+
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect('/urls');
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
