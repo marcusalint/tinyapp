5fdbf9935332c189050be835ed437c73bb0064c1
salar taeb <salartaeb10@gmail.com>
2020-11-16 06:32:23 +0000

    included getUserByEmail

---
+++ helpers.js
@@ -0,0 +1,11 @@
+const getUserByEmail = function(email, userDatabase) {
+  for (const user in userDatabase) {
+    if (userDatabase[user].email === email) {
+      return userDatabase[user].id;
+    }
+  }
+};
+
+module.exports = {
+  getUserByEmail
+}
