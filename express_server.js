ae9e9a1b29f4884855182bd4aa07067a0b270c1e
salar taeb <salartaeb10@gmail.com>
2020-11-23 13:50:44 +0000

    moved helper functions to helper directory

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ const PORT = 3000;
 const bcrypt = require('bcrypt');
 const cookieSession = require("cookie-session");
 const bodyParser = require("body-parser");
+const { checkEmailDatabase, generateRandomString, getUsersByEmail, urlsForUser } = require('./helpers.js');
 
 
 app.use(cookieSession({
@@ -221,43 +222,6 @@ app.post("/register", (req, res) => {
 
 
 
-// ------------------------------------------ FUNCTIONS ----------------------->
-
-// Generate Random String Function
-function generateRandomString(length) {
-  let string = "";
-  length = 6;
-  const charset = "abcdefghijklmnopqrstuvwxyz0123456789";
-
-  for( let i=0; i < length; i++ )
-        string += charset.charAt(Math.floor(Math.random() * charset.length));
-
-  return string;
-}
-
-
-// Check If Email Already Exists Function
- function checkEmailDatabase(email, exists) {
-  let emailExists = false;
-
-  for (let user in users) {
-    if (email === users[user]["email"]) {
-      emailExists = true;
-    }
-  }
-  return emailExists;
-}
-
- // GET URLS FOR USER
- const urlsForUser = function(id, urlDatabase) {
-  const userUrls = {};
-  for (let shortURL in urlDatabase) {
-    if (urlDatabase[shortURL].userID === id) {
-      userUrls[shortURL] = urlDatabase[shortURL];
-    }
-  }
-  return userUrls;
-}
 
 
 
