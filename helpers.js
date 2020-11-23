44c6b3d7adb6a94fd0480cd42cd3f5091a2126ab
salar taeb <salartaeb10@gmail.com>
2020-11-23 13:51:19 +0000

    included generateRandomString, urlsForUsers, and other helper functions

--- helpers.js
+++ helpers.js
@@ -6,6 +6,46 @@ const getUserByEmail = function(email, userDatabase) {
   }
 };
 
+
+// Generate Random String Function
+function generateRandomString(length) {
+  let string = "";
+  length = 6;
+  const charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+  for( let i=0; i < length; i++ )
+        string += charset.charAt(Math.floor(Math.random() * charset.length));
+
+  return string;
+}
+
+
+// Check If Email Already Exists Function
+ function checkEmailDatabase(email, exists) {
+  let emailExists = false;
+
+  for (let user in users) {
+    if (email === users[user]["email"]) {
+      emailExists = true;
+    }
+  }
+  return emailExists;
+}
+
+ // GET URLS FOR USER
+ const urlsForUser = function(id, urlDatabase) {
+  const userUrls = {};
+  for (let shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === id) {
+      userUrls[shortURL] = urlDatabase[shortURL];
+    }
+  }
+  return userUrls;
+}
+
 module.exports = {
-  getUserByEmail
+  getUserByEmail,
+  urlsForUser,
+  checkEmailDatabase,
+  generateRandomString
 }
